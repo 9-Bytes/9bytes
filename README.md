262e23e070f10cb24d04306e579fa0d899fee493
gh05d <24304457+Gh05d@users.noreply.github.com>
2023-03-28 14:28:47 +0200

    Fixed bug in build script

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/c58385e2-dc2a-4d2f-b327-8a318ed1a4b1/deploy-status)](https://app.netlify.com/sites/9bytes/deploys)
--- build.mjs
+++ build.mjs
@@ -13,9 +13,10 @@ fs.copySync("robots.txt", "build/robots.txt");
 const htmlFiles = ["index", "datenschutz", "impressum"];
 
 for (const fileName of htmlFiles) {
-	const html = fs.readFileSync(`build/${fileName}.html`);
+	const file = `build/${fileName}.html`;
+	const html = fs.readFileSync(file);
 	const $ = cheerio.load(html);
 
 	$("link[rel='stylesheet']").attr("href", "style.min.css");
-	fs.writeFileSync("build/index.html", $.html());
+	fs.writeFileSync(file, $.html());
 }
